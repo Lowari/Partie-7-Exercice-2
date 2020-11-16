74a2d2e34598438373e68c093667c6035e5e34b8
Lowari <66225181+Lowari@users.noreply.github.com>
2020-11-16 15:15:02 +0100

    Partie 7 - Exercice 2

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let fables = document.querySelectorAll('a');
+
+fables.forEach(fable => {
+        fable.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(fable.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
